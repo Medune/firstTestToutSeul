1b07dbfd7c5f582935031400725c82ca506319f9
Maxime <maxime2.goguet@gmail.com>
2020-02-27 16:56:46 +0100

    Test envoi

---
+++ src/com/inti/entities/Departement.java
@@ -0,0 +1,45 @@
+package com.inti.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Departement {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idDepartement;
+	private int codeDepartement;
+	private String nomDepartement;
+
+	public Departement() {
+
+	}
+
+	public Long getIdDepartement() {
+		return idDepartement;
+	}
+
+	public void setIdDepartement(Long idDepartement) {
+		this.idDepartement = idDepartement;
+	}
+
+	public int getCodeDepartement() {
+		return codeDepartement;
+	}
+
+	public void setCodeDepartement(int codeDepartement) {
+		this.codeDepartement = codeDepartement;
+	}
+
+	public String getNomDepartement() {
+		return nomDepartement;
+	}
+
+	public void setNomDepartement(String nomDepartement) {
+		this.nomDepartement = nomDepartement;
+	}
+
+}
